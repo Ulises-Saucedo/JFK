62cf4919166e68f83a2e74b6616fe78e4c9379ee
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2023-10-30 21:57:52 -0300

    first commit

--- src/views/historia/historia.php
+++ src/views/historia/historia.php
@@ -118,97 +118,103 @@
         </div>
     </header>
     <section>
-        <div class="timeline-container" id="timeline-1">
-            <div class="timeline-header">
-                <h2 class="timeline-header__title">Nuestra Historia</h2>
-                <h3 class="timeline-header__subtitle">E.E.S.T.N°5 
-                JOHN F. KENNEDY</h3>
-            </div>
-            <div class="timeline">
-                <div class="timeline-item" data-text="CREACIÓN DE ESCUELAS DE ARTES Y OFICIOS">
-                    <div class="timeline__content"><img class="timeline__img" src="fotos/15.jpeg"/>
-                    <h2 class="timeline__content-title">1943</h2>
-                    <p class="timeline__content-desc">El 17 de abril de 1943, el Poder Ejecutivo Nacional resolvió, mediante el decreto N°148065, la creación de Escuelas de Artes y Oficios con el propósito de proporcionar formación y preparación a obreros calificados en diversas actividades industriales.</p>
-                </div>
-            </div>
-            <div class="timeline-item" data-text="EXPANSIÓN DE LA ESCUELA">
-                <div class="timeline__content"><img class="timeline__img" src="http://gazetemanifesto.com/wp-content/uploads/2015/11/mustafa-kemal.jpg"/>
-                    <h2 class="timeline__content-title">1944</h2>
-                    <p class="timeline__content-desc">En 1944, la Escuela ya tenía 150 alumnos y necesitó habilitar un nuevo taller. El Sr. Juan Rassetto puso a disposición tres casas destinadas a vivienda que fueron adaptadas para la instalación del establecimiento.</p>
-                </div>
-            </div>
-            <div class="timeline-item" data-text="NUEVA UBICACIÓN">
-                <div class="timeline__content"><img class="timeline__img" src="http://i.cdn.ensonhaber.com/resimler/diger/ataturk_3473.jpg"/>
-                    <h2 class="timeline__content-title">1945</h2>
-                    <p class="timeline__content-desc">En 1945, la Escuela se trasladó a su nuevo y cómodo local, con amplios talleres modernamente equipados para la época. Este local estaba ubicado en la calle Basavilbaso 2073, en Lanús Este.</p>
-                </div>
-            </div>
-            <div class="timeline-item" data-text="CONVERSIÓN EN ESCUELA INDUSTRIAL">
-                <div class="timeline__content"><img class="timeline__img" src="http://cdn.yemek.com/uploads/2014/11/ataturk-10-kasim.jpg"/>
-                    <h2 class="timeline__content-title">1952</h2>
-                    <p class="timeline__content-desc">En 1952, la escuela se convirtió en la Escuela Industrial de la Nación, especializándose en Mecánica en su ciclo superior.</p>
-                </div>
-            </div>
-            <div class="timeline-item" data-text="CREACIÓN DE LA COOPERATIVA">
-                <div class="timeline__content"><img class="timeline__img" src="fotos/coperativa.jpeg"/>
-                    <h2 class="timeline__content-title">1959</h2>
-                    <p class="timeline__content-desc">En agosto de 1959, se formó la Cooperativa de Producción, Consumo y Edificación de la Escuela Industrial de la Nación Lanús Ltda.  Esta cooperativa tenía como objetivo principal construir el edificio necesario para el establecimiento y equipar sus instalaciones. </p>
-            </div>
-            </div>
-                <div class="timeline-item" data-text="CAMBIO DE NOMBRE">
-                    <div class="timeline__content"><img class="timeline__img" src="https://cdn.britannica.com/32/172732-138-941D1E2C/overview-John-F-Kennedy.jpg"/>
-                        <h2 class="timeline__content-title">1964</h2>
-                        <p class="timeline__content-desc">En homenaje al presidente de los Estados Unidos, asesinado en 1963, John Fitzgerald Kennedy, las autoridades de la escuela, y las del C.O.N.E.T., decidieron instituir su nombre al establecimieto en un acto realizado el 4 de julio de 1964.</p>
-                    </div>
-                </div>
-                <div class="timeline-item" data-text="EXPANSIÓN Y NUEVA ESPECIALIDAD">
-                    <div class="timeline__content"><img class="timeline__img" src="fotos/2.jpeg"/>
-                        <h2 class="timeline__content-title">1968</h2>
-                        <p class="timeline__content-desc">En 1968, comenzó a funcionar el taller en el nuevo edificio. Ocupaba las dos aulas que hoy son laboratorios del ciclo superior, ya que todavía no estaba terminado el techo que cubre los talleres. Al año siguiente se mudó totalmente el taller y se habilitaron en el piso superior la preceptoría y dos aulas de dibujo. Simultáneamente se incorporaba una nueva especialidad: Electromecánica.</p>
-                    </div>
-                </div>
-                <div class="timeline-item" data-text="CREACIÓN Y APERTURA DEL ANEXO">
-                    <div class="timeline__content"><img class="timeline__img" src="fotos/0.jpeg"/>
-                        <h2 class="timeline__content-title">1984</h2>
-                        <p class="timeline__content-desc">
-                        En diciembre de 1984, se creó el Anexo de la E.N.E.T. Nº1 como solución a la creciente demanda de estudiantes. Y en mayo de 1986, el Anexo abrió sus puertas con cuatro divisiones de primer año y dos de segundo año.
-                        </p>
-                    </div>
-                </div>
-                <div class="timeline-item" data-text="TRANSFORMACIÓN EN ESCUELA Nº3">
-                    <div class="timeline__content"><img class="timeline__img" src="fotos/0.jpeg"/>
-                        <h2 class="timeline__content-title">1988</h2>
-                        <p class="timeline__content-desc">
-                        En 1988, el Anexo se convirtió en la Escuela Nacional de Educación Técnica Nº3 de Lanús, especializándose en Electrónica.
-                    </p>
-                    </div>
-                </div>
-                <div class="timeline-item" data-text="INCORPORACIÓN DE LA INFORMÁTICA">
-                    <div class="timeline__content"><img class="timeline__img" src="fotos/1.jpeg"/>
-                        <h2 class="timeline__content-title">1997</h2>
-                        <p class="timeline__content-desc">
-                        En 1997, la escuela agregó la modalidad Informática, abriendo nuevas divisiones y aumentando las vacantes.
-                        </p>
-                    </div>
-                </div>
-                <div class="timeline-item" data-text="RECUPERACIÓN DE ALUMNOS DE 8º Y 9º AÑO">
-                    <div class="timeline__content"><img class="timeline__img" src="fotos/14.jpeg"/>
-                        <h2 class="timeline__content-title">2004</h2>
-                        <p class="timeline__content-desc">
-                        En 2004, se recuperó la pertenencia de los alumnos de 8º y 9º año, y en 2005 se incorporó el 7º año.
-                        </p>
-                    </div>
-                </div>
-                <div class="timeline-item" data-text="UNIDAD ACADÉMICA">
-                    <div class="timeline__content"><img class="timeline__img" src="fotos/0.jpeg"/>
-                        <h2 class="timeline__content-title">ACTUALIDAD</h2>
-                        <p class="timeline__content-desc">
-                        Actualmente, la escuela opera como una única unidad académica con alrededor de 1100 alumnos en Ciclo Básico y Ciclo Superior, especializándose en Electromecánica, Programacion e Informática.
-                        </p>
-                    </div>
-                </div>
-            </div>
+      <div class="timeline-container" id="timeline-1">
+        <div class="timeline-header">
+          <h2 class="timeline-header__title">Nuestra Historia</h2>
+          <h3 class="timeline-header__subtitle">E.E.S.T.N°5 
+            JOHN F. KENNEDY</h3>
         </div>
+        <div class="timeline">
+          <div class="timeline-item" data-text="CREACIÓN DE ESCUELAS DE ARTES Y OFICIOS">
+            <div class="timeline__content"><img class="timeline__img" src="fotos/pioneros.jpg"/>
+              <h2 class="timeline__content-title">1943</h2>
+              <p class="timeline__content-desc">El 17 de abril de 1943, el Poder Ejecutivo Nacional resolvió, mediante el decreto N°148065, la creación de Escuelas de Artes y Oficios con el propósito de proporcionar formación y preparación a obreros calificados en diversas actividades industriales.</p>
+            </div>
+          </div>
+          <div class="timeline-item" data-text="EXPANSIÓN DE LA ESCUELA">
+            <div class="timeline__content"><img class="timeline__img" src="fotos/patio1945.jpg"/>
+              <h2 class="timeline__content-title">1944</h2>
+              <p class="timeline__content-desc">En 1944, la Escuela ya tenía 150 alumnos y necesitó habilitar un nuevo taller. El Sr. Juan Rassetto puso a disposición tres casas destinadas a vivienda que fueron adaptadas para la instalación del establecimiento.</p>
+            </div>
+          </div>
+          <div class="timeline-item" data-text="NUEVA UBICACIÓN">
+            <div class="timeline__content"><img class="timeline__img" src="fotos/ubi.jpg"/>
+              <h2 class="timeline__content-title">1945</h2>
+              <p class="timeline__content-desc">En 1945, la Escuela se trasladó a su nuevo y cómodo local, con amplios talleres modernamente equipados para la época. Este local estaba ubicado en la calle Basavilbaso 2073, en Lanús Este.</p>
+            </div>
+          </div>
+          <div class="timeline-item" data-text="CONVERSIÓN EN ESCUELA INDUSTRIAL">
+            <div class="timeline__content"><img class="timeline__img" src="fotos/industrial.jpg"/>
+              <h2 class="timeline__content-title">1952</h2>
+              <p class="timeline__content-desc">En 1952, la escuela se convirtió en la Escuela Industrial de la Nación, especializándose en Mecánica en su ciclo superior.</p>
+            </div>
+          </div>
+          <div class="timeline-item" data-text="CREACIÓN DE LA COOPERATIVA">
+            <div class="timeline__content"><img class="timeline__img" src="fotos/coperativa.jpeg"/>
+              <h2 class="timeline__content-title">1959</h2>
+              <p class="timeline__content-desc">En agosto de 1959, se formó la Cooperativa de Producción, Consumo y Edificación de la Escuela Industrial de la Nación Lanús Ltda.  Esta cooperativa tenía como objetivo principal construir el edificio necesario para el establecimiento y equipar sus instalaciones. </p>
+            </div>
+          </div>
+          <div class="timeline-item" data-text="CAMBIO DE NOMBRE">
+            <div class="timeline__content"><img class="timeline__img" src="https://cdn.britannica.com/32/172732-138-941D1E2C/overview-John-F-Kennedy.jpg"/>
+              <h2 class="timeline__content-title">1964</h2>
+              <p class="timeline__content-desc">En homenaje al presidente de los Estados Unidos, asesinado en 1963, John Fitzgerald Kennedy, las autoridades de la escuela, y las del C.O.N.E.T., decidieron instituir su nombre al establecimieto en un acto realizado el 4 de julio de 1964.</p>
+            </div>
+          </div>
+          <div class="timeline-item" data-text="EXPANSIÓN Y NUEVA ESPECIALIDAD">
+            <div class="timeline__content"><img class="timeline__img" src="fotos/2.jpeg"/>
+              <h2 class="timeline__content-title">1968</h2>
+              <p class="timeline__content-desc">En 1968, comenzó a funcionar el taller en el nuevo edificio. Ocupaba las dos aulas que hoy son laboratorios del ciclo superior, ya que todavía no estaba terminado el techo que cubre los talleres. Al año siguiente se mudó totalmente el taller y se habilitaron en el piso superior la preceptoría y dos aulas de dibujo. Simultáneamente se incorporaba una nueva especialidad: Electromecánica.</p>
+            </div>
+          </div>
+          <div class="timeline-item" data-text="CREACIÓN Y APERTURA DEL ANEXO">
+            <div class="timeline__content"><img class="timeline__img" src="fotos/eet1.jpg"/>
+              <h2 class="timeline__content-title">1984</h2>
+              <p class="timeline__content-desc">
+                En diciembre de 1984, se creó el Anexo de la E.N.E.T. Nº1 como solución a la creciente demanda de estudiantes. Y en mayo de 1986, el Anexo abrió sus puertas con cuatro divisiones de primer año y dos de segundo año.
+              </p>
+            </div>
+          </div>
+          <div class="timeline-item" data-text="TRANSFORMACIÓN EN ESCUELA Nº3">
+            <div class="timeline__content"><img class="timeline__img" src="fotos/15.jpeg"/>
+              <h2 class="timeline__content-title">1988</h2>
+              <p class="timeline__content-desc">
+                En 1988, el Anexo se convirtió en la Escuela Nacional de Educación Técnica Nº3 de Lanús, especializándose en Electrónica.
+              </p>
+            </div>
+          </div>
+          <div class="timeline-item" data-text="INCORPORACIÓN DE LA INFORMÁTICA">
+            <div class="timeline__content"><img class="timeline__img" src="fotos/1.jpeg"/>
+              <h2 class="timeline__content-title">1997</h2>
+              <p class="timeline__content-desc">
+                En 1997, la escuela agregó la modalidad Informática, abriendo nuevas divisiones y aumentando las vacantes.
+              </p>
+            </div>
+          </div>
+          <div class="timeline-item" data-text="RECUPERACIÓN DE ALUMNOS DE 8º Y 9º AÑO">
+            <div class="timeline__content"><img class="timeline__img" src="fotos/14.jpeg"/>
+              <h2 class="timeline__content-title">2004</h2>
+              <p class="timeline__content-desc">
+                En 2004, se recuperó la pertenencia de los alumnos de 8º y 9º año, y en 2005 se incorporó el 7º año.
+              </p>
+            </div>
+          </div>
+          <div class="timeline-item" data-text="UNIDAD ACADÉMICA">
+            <div class="timeline__content"><img class="timeline__img" src="fotos/0.jpeg"/>
+              <h2 class="timeline__content-title">ACTUALIDAD</h2>
+              <p class="timeline__content-desc">
+                Actualmente, la escuela opera como una única unidad académica con alrededor de 1100 alumnos en Ciclo Básico y Ciclo Superior, especializándose en Electromecánica, Programacion e Informática.
+              </p>
+            </div>
+          </div>
+        </div>
+    </div>
+    </section>
+    <section>
+      <div class="cont-pdf">
+        <p>Accedé a nuestra historia completa mediante el siguiente enlace:</p>
+        <a href="eet5_historia.pdf" target="blank">Historia completa</a>
+      </div>
     </section>
     <footer>
         <div class="footer__container">
