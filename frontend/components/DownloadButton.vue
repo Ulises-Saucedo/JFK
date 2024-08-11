5c1fd25118fb8f68698738b6ae5d73f0aafb5609
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-08-11 10:44:37 -0300

    Componente de UI agregado y archivos necesarios para las vistas.

---
+++ frontend/components/DownloadButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps({
+  href: {
+    type: String,
+    required: true,
+  },
+  download: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <a
+    role="button"
+    class="bg-blue-500 ring-blue-600 ring-1 rounded-md hover:bg-blue-600 ease-in-out duration-200 block text-center text-white px-4 py-2"
+    :href="href"
+    :download="download"
+  >
+    {{ text }}
+  </a>
+</template>
