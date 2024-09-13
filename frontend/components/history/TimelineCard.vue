4210cecc105605e97a8deeea0e9426f4e614bae8
Ulises <ulisesaucedo.se04@gmail.com>
2024-09-13 15:37:35 -0300

    history page

---
+++ frontend/components/history/TimelineCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active"
+  >
+    <div
+      class="flex items-center justify-center w-10 h-10 rounded-full bg-blue-300 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"
+    >
+      <img src="/logotipo.png" class="w-6 h-6" />
+    </div>
+    <div
+      class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow"
+    >
+      <div class="flex items-center justify-between space-x-2 mb-1">
+        <div class="font-bold">
+          <slot name="date" />
+        </div>
+      </div>
+      <div class="text-black/75">
+        <slot name="content" />
+      </div>
+    </div>
+  </div>
+</template>
--- frontend/pages/historia.vue
+++ frontend/pages/historia.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import TimelineCard from "~/components/history/TimelineCard.vue";
+import { timelineItems } from "~/constants";
+
 useHead({
   title: "Historia",
 });
@@ -6,39 +9,30 @@ useHead({
 
 <template>
   <div class="my-16 max-w-screen-md mx-auto px-8">
-    <h2 class="text-black text-5xl font-bold text-center">Nuestra historia</h2>
-    <section class="space-y-4 my-8">
-      <p class="text-black">
-        La Escuela Técnica de Oficios de Lanús fue creada en 1943 con el
-        objetivo de formar obreros calificados en diversas actividades
-        industriales. Inició sus clases en 1944 con 70 alumnos y rápidamente
-        creció en número, lo que llevó a la necesidad de expandir sus
-        instalaciones. En 1945, la escuela se trasladó a un nuevo edificio en
-        Lanús Este, donde comenzó a ofrecer formación en mecánica, otorgando
-        títulos de Experto Mecánico y Experto Calificado Mecánico.
-      </p>
-      <p class="text-black">
-        A lo largo de los años, la escuela evolucionó y se consolidó como un
-        referente en educación técnica en la región. En 1952, se transformó en
-        la Escuela Industrial de la Nación con especialidad en Mecánica, y en
-        1968 incorporó la especialidad de Electromecánica. El crecimiento
-        continuo llevó a la creación de un nuevo edificio, inaugurado en 1973, y
-        posteriormente se anexó un campo de deportes en 1982.
-      </p>
-      <p class="text-black">
-        En 1988, el Anexo de la escuela se convirtió en la Escuela Nacional de
-        Educación Técnica Nº3 de Lanús, con especialidad en Electrónica. En
-        1997, la escuela sumó la especialidad de Informática, adaptándose a los
-        cambios educativos de la época. Hoy, la institución cuenta con más de
-        1100 alumnos, ofreciendo formación técnica en Electromecánica e
-        Informática, y sigue destacándose por su calidad educativa en
-        evaluaciones a nivel regional y provincial.
-      </p>
-    </section>
-    <DownloadButton
-      href="/files/eet5_historia.pdf"
-      download="historia.pdf"
-      text="Descargar historia en PDF"
-    />
+    <h2 class="text-5xl font-bold text-center">Nuestra historia</h2>
+
+    <div
+      class="my-16 space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent"
+    >
+      <TimelineCard v-for="(item, idx) in timelineItems" :key="idx">
+        <template #date>{{ item.date }}</template>
+        <template #content>
+          {{ item.content }}
+        </template>
+      </TimelineCard>
+    </div>
+
+    <div class="w-full flex justify-center">
+      <Button class="bg-blue-600 hover:bg-blue-500" as-child>
+        <a
+          class="flex items-center"
+          href="/files/eet5_historia.pdf"
+          download="historia.pdf"
+        >
+          <Icon class="mr-2 text-lg" name="tabler:download" />
+          <span>Descargar historia completa</span>
+        </a>
+      </Button>
+    </div>
   </div>
 </template>
