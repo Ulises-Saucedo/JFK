b57a0889e4b5b48db200a0af50928d887bbe36a4
Ulises <ulisesaucedo.se04@gmail.com>
2024-09-13 15:37:19 -0300

    timeline content

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -39,3 +39,77 @@ export const testimonials: Array<Testimonial> = [
       '"La tecnicatura en Informática me permitió entender el funcionamiento interno de los sistemas y cómo protegerlos. Gracias a los conocimientos adquiridos, hoy trabajo en la administración de redes, asegurando que la infraestructura de las empresas sea segura y eficiente. Es un rol desafiante, pero las bases que obtuve en la tecnicatura me dieron la confianza para enfrentar cualquier reto."',
   },
 ];
+
+export const timelineItems = [
+  {
+    date: "1945",
+    content:
+      "Se traslada a un nuevo edificio en la calle Basavilbaso 2073, Lanús Este.",
+  },
+  {
+    date: "1949",
+    content:
+      "Se plantea la necesidad de ampliar el espacio físico de la escuela.",
+  },
+  {
+    date: "1952",
+    content:
+      "La escuela se transforma en la Escuela Industrial de la Nación con la especialidad Mecánica en su ciclo superior.",
+  },
+  {
+    date: "1959",
+    content:
+      "Creación de la Cooperativa de Producción para la construcción del nuevo edificio.",
+  },
+  {
+    date: "1963",
+    content: "Se coloca la piedra fundamental del nuevo edificio.",
+  },
+  {
+    date: "1964",
+    content:
+      'La escuela adopta el nombre de E.N.E.T. Nº1 "John Fitzgerald Kennedy".',
+  },
+  {
+    date: "1968-1969",
+    content: "Comienzan a funcionar los talleres y se habilitan nuevas aulas.",
+  },
+  {
+    date: "1973",
+    content:
+      "Inauguración del nuevo edificio con 16 aulas, talleres y laboratorios.",
+  },
+  {
+    date: "1982",
+    content: "Se anexa un campo de deportes de 10,600 m².",
+  },
+  {
+    date: "1984-1985",
+    content:
+      "Se crea el Anexo de la E.N.E.T. Nº1 debido al aumento de alumnos.",
+  },
+  {
+    date: "1988",
+    content:
+      "El Anexo se convierte en la E.N.E.T. Nº3 con la especialidad Electrónica.",
+  },
+  {
+    date: "1995",
+    content:
+      "Se implementa la Ley Federal de Educación en la provincia de Buenos Aires.",
+  },
+  {
+    date: "1997",
+    content:
+      "Se incorpora la modalidad Informática y se eliminan divisiones de Mecánica.",
+  },
+  {
+    date: "2004-2005",
+    content: "Se recuperan los alumnos de 7º, 8º y 9º año.",
+  },
+  {
+    date: "2009",
+    content:
+      "Se inician evaluaciones anuales por capacidades con altas calificaciones en la escuela.",
+  },
+];
