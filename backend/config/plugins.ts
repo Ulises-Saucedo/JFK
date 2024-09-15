1acdcf8505afc28c71035118fcd89da72d29e1da
Ulises <ulisesaucedo.se04@gmail.com>
2024-09-15 18:43:19 -0300

    firebase storage added

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+import { firebaseStorageConfig } from "./firebase/firebase";
+
+export default ({ env }) => {
+  return {
+    upload: {
+      config: {
+        provider: "strapi-provider-firebase-storage",
+        providerOptions: {
+          serviceAccount: JSON.parse(JSON.stringify(firebaseStorageConfig)),
+          bucket: env("STORAGE_BUCKET_URL", "my-bucket-name.appspot.com"),
+          sortInStorage: true,
+          debug: false,
+        },
+      },
+    },
+  };
+};
